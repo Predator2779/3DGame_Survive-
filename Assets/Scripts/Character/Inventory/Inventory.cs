58b0de95fd3d176f46b66f21b368b26f520a71a8
Predator2779 <predator2779@gmail.com>
2023-10-24 19:21:14 +0500

    ---

--- Assets/Scripts/Character/Inventory/Inventory.cs
+++ Assets/Scripts/Character/Inventory/Inventory.cs
@@ -6,12 +6,13 @@ namespace Character.Inventory
 {
     public class Inventory : MonoBehaviour
     {
-        [SerializeField] private List<Item> _items = new();
         [SerializeField] private InventoryDrawer _invDrawer;
+        [SerializeField, Min(1)] private int _size;
+        [SerializeField] private List<Item> _items = new();
 
         public void AddItem(Item item)
         {
-            if (!_items.Contains(item)) _items.Add(item);
+            if (!_items.Contains(item) && _items.Count < _size) _items.Add(item);
         }
 
         public void RemoveItem(Item item)
