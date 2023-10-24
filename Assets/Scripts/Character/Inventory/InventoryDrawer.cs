65eef2004cd4d269ebd48072ab79691e8860ddf3
Predator2779 <predator2779@gmail.com>
2023-10-24 15:31:46 +0500

    fixed inventory

--- Assets/Scripts/Character/Inventory/InventoryDrawer.cs
+++ Assets/Scripts/Character/Inventory/InventoryDrawer.cs
@@ -5,18 +5,38 @@ using UnityEngine.UI;
 public class InventoryDrawer : MonoBehaviour
 {
     [SerializeField] private RectTransform _inventoryPanel;
-    
+
+    private bool _isDisplayed;
+
     public void DisplayInventory(Item[] items)
     {
+        if (_isDisplayed)
+        {
+            ClearInventory();
+            
+            _isDisplayed = false;
+            _inventoryPanel.gameObject.SetActive(false);
+            
+            return;
+        }
+
+        _isDisplayed = true;
         _inventoryPanel.gameObject.SetActive(true);
         
         for (int i = 0; i < items.Length; i++)
         {
             var item = items[i];
             var icon = new GameObject("Icon");
-            
+
             icon.AddComponent<Image>().sprite = item.GetData().GetIcon();
             icon.transform.SetParent(_inventoryPanel);
         }
+    }
+
+    private void ClearInventory()
+    {
+        int length = _inventoryPanel.childCount;
+
+        for (int i = 0; i < length; i++) Destroy(_inventoryPanel.GetChild(i).gameObject);
     }
 }
