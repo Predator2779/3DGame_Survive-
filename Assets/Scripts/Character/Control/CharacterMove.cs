cd5024d6175089f4215c8d4fb128093bd5da973d
Predator2779 <predator2779@gmail.com>
2023-10-28 23:41:09 +0500

    minor changes

--- Assets/Scripts/Character/Control/CharacterMove.cs
+++ Assets/Scripts/Character/Control/CharacterMove.cs
@@ -1,40 +1,14 @@
-using System;
 using UnityEngine;
 using UnityEngine.AI;
-using EventHandler = General.EventHandler;
 
 namespace Character.Control
 {
     public class CharacterMove : MonoBehaviour
     {
-        [SerializeField] private Camera _cam;
-
         private NavMeshAgent _agent;
-        private bool _canMove;
 
-        private void Start()
-        {
-            _agent = GetComponent<NavMeshAgent>();
+        private void Start() => _agent = GetComponent<NavMeshAgent>();
 
-            EventHandler.OnInventoryButtonUp.AddListener(SwitchMove);
-        }
-
-        public void Move()
-        {
-            if (_canMove) return;
-
-            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
-                _agent.SetDestination(hit.point);
-        }
-
-        private void SwitchMove()
-        {
-            if (!_canMove) _canMove = true;
-            else _canMove = false;
-        }
-
-        private void OnDestroy() => EventHandler.OnInventoryButtonUp.RemoveListener(SwitchMove);
+        public void Move(Vector3 destination) => _agent.SetDestination(destination);
     }
 }
--- Assets/Scripts/Character/Control/InputHandler.cs
+++ Assets/Scripts/Character/Control/InputHandler.cs
@@ -8,22 +8,31 @@ namespace Character.Control
     public class InputHandler : MonoBehaviour
     {
         [SerializeField] private Person _person;
+        [SerializeField] private Camera _cam;
         [SerializeField] private InventoryDrawer _invDrawer;
 
-        private void OnValidate() => _person ??= GetComponent<Person>();
+        private void Start() => _person ??= GetComponent<Person>();
 
         private void Update() => CheckKeys();
 
         private void CheckKeys()
         {
-            if (Input.GetMouseButtonUp(0)) _person.GetCharacterMove().Move();
-            
+            if (Input.GetMouseButtonUp(0)) MoveCharacter();
+
             if (Input.GetKeyUp(KeyCode.Tab))
             {
                 _invDrawer.DisplayInventory(_person);
-                
+
                 EventHandler.OnInventoryButtonUp?.Invoke();
             }
+        }
+
+        private void MoveCharacter()
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+                _person.GetCharacterMove().Move(hit.point);
         }
     }
 }
